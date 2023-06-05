90aaebe6c03edd7be3ce7a01203d74b1dbd0300e
Edith <rissafonseca@yahoo.com>
2023-06-05 18:43:08 -0500

    completed homepage

--- public/js/login.js
+++ public/js/login.js
@@ -1,56 +1,47 @@
-// Already registered user handler
-const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the login form
-    const email = document.querySelector("#email-login").value.trim();
-    const password = document.querySelector("#password-login").value.trim();
-  
-    if (email && password) {
-      // Send a POST request to the API endpoint
-      const response = await fetch("/api/users/login", {
-        method: "POST",
-        body: JSON.stringify({ email, password }),
-        headers: { "Content-Type": "application/json" },
-      });
-  
-      if (response.ok) {
-        // If successful, redirect the browser to the home page
-        document.location.replace("/");
-      } else {
-        alert(response.statusText);
-      }
-    }
+// Function to clear the form inputs
+const clearFormInputs = () => {
+    document.querySelector("#display_name").value = "";
+    document.querySelector("#email-login").value = "";
+    document.querySelector("#new-password-login").value = "";
   };
   
-  // Sign up handler
+  // Signup form handler
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector("#name-signup").value.trim();
-    const email = document.querySelector("#email-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
+    const displayName = document.querySelector("#display_name").value.trim();
+    const email = document.querySelector("#email-login").value.trim();
+    const password = document.querySelector("#new-password-login").value.trim();
   
-    if (name && email && password) {
+    if (displayName && email && password) {
       const response = await fetch("/api/users", {
         method: "POST",
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ displayName, email, password }),
         headers: { "Content-Type": "application/json" },
       });
   
       if (response.ok) {
-        document.location.replace("/");
+        // Get the display name from the response
+        const responseData = await response.json();
+        const savedDisplayName = responseData.display_name;
+  
+        // Redirect the user to the home page with the display name displayed on the upper right corner
+        const baseURL = window.location.origin; // Get the base URL
+        const homeURL = `${baseURL}/`; // Construct the home page URL
+        window.location.href = homeURL + "?display_name=" + encodeURIComponent(savedDisplayName);
       } else {
         alert(response.statusText);
       }
     }
   };
   
-  // Event Listeners
-  document
-    .querySelector(".login-form")
-    .addEventListener("submit", loginFormHandler);
+  // Add event listener to the signup form
+  document.querySelector(".signup-form").addEventListener("submit", signupFormHandler);
   
-  document
-    .querySelector(".signup-form")
-    .addEventListener("submit", signupFormHandler);
+  // Call the clearFormInputs function when the page loads
+  window.addEventListener("load", clearFormInputs);
+  
+  // Prevent form field auto-fill by adding autocomplete="off"
+  document.querySelector("#display_name").setAttribute("autocomplete", "off");
+  document.querySelector("#email-login").setAttribute("autocomplete", "off");
+  document.querySelector("#new-password-login").setAttribute("autocomplete", "off");
