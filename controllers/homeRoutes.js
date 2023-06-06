88c074e4da8b8cb3cbfdfdd11369f44dedaf82df
ctalv <122413805+ctalv@users.noreply.github.com>
2023-06-05 21:09:58 -0500

    added homepage recipe route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,8 @@ const fetch = require('node-fetch');
 const withAuth = require('../utils/auth');
 require('dotenv').config();
 
+const { Recipe, User } = require('../models');
+
 apiKey = process.env.API_KEY;
 const mainCourseQuery = 'main course';
 const addRecipeInformation = true;
@@ -10,32 +12,72 @@ const fillIngredients = true;
 
 const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${mainCourseQuery}&addRecipeInformation=${addRecipeInformation}&fillIngredients=${fillIngredients}`;
 
+// display recipes
 router.get('/', async (req, res) => {
   try {
-
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
+        // console.log(data);
+        // id, title, ingredients, instruction, photo
+        const recipes = [];
+        for (let i = 0; i < data.results.length; i++) {
+          let recipeNum = data.results[i];
+
+          let id = recipeNum.id;
+          let title = recipeNum.title;
+          let image = recipeNum.image;
+          let instructionsAll = recipeNum.analyzedInstructions[0].steps
+          let ingredientsAll = recipeNum.missedIngredients;
+          let ingredientArr = [];
+          let instructionArr = [];
+
+          for (let j = 0; j < ingredientsAll.length; j++) {
+            ingredientTotal = `${ingredientsAll[j].amount} ${ingredientsAll[j].unitShort} ${ingredientsAll[j].name}`
+            ingredientArr.push(ingredientTotal)
+          }
+
+          ingredientArr = ingredientArr.join(' ; ')
+
+          for (let j = 0; j < instructionsAll.length; j++) {
+            let step = instructionsAll[j].step
+            instructionArr.push(step)
+          }
+          instructionArr = instructionArr.join(' ; ')
+
+          // console.log(ingredientArr);
+          // console.log(instructionArr);
+          // console.log(id, title, image);
+
+          let recipe = {
+            id: id,
+            title: title,
+            image: image,
+            ingredients: ingredientArr,
+            instructions: instructionArr
+          };
+
+          recipes.push(recipe); // Add each recipe to the recipes array
+        }
+
+        // console.log(recipes)
+        res.render('homepage', { 
+          recipes: recipes,
+         }); 
+        // res.status(200).json(recipes);
       })
       .catch(error => {
         console.log('Error:', error);
       });
-
-    // Serialize data so the template can read it
-    // const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage'); 
-    //   projects, 
-    // });
 
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+
+
+router.get('/recipe/:id', async (req, res) => {
   try {
     const projectData = await Project.findByPk(req.params.id, {
       include: [
@@ -58,6 +100,7 @@ router.get('/project/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
+// favorited routes
 router.get('/profile', async (req, res) => {
   try {
     // Find the logged in user based on the session ID
