dce1c7b62ac72a9217ae0b656d7738082122511d
cnguyen1013 <CNguyen7@mdanderson.org>
2023-06-05 19:43:54 -0500

    handlebars development

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,89 @@
+const router = require('express').Router();
+const fetch = require('node-fetch');
+const withAuth = require('../utils/auth');
+require('dotenv').config();
+
+apiKey = process.env.API_KEY;
+const mainCourseQuery = 'main course';
+const addRecipeInformation = true;
+const fillIngredients = true;
+
+const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${mainCourseQuery}&addRecipeInformation=${addRecipeInformation}&fillIngredients=${fillIngredients}`;
+
+router.get('/', async (req, res) => {
+  try {
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+
+    // Serialize data so the template can read it
+    // const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage'); 
+    //   projects, 
+    // });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/project/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    // res.render('project', {
+    //   ...project,
+    //   logged_in: req.session.logged_in
+    // });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    // res.render('profile', {
+    //   ...user,
+    //   logged_in: true
+    // });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+});
+
+module.exports = router;
